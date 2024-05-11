585563130d4d91ad230d4f337447c150eabfd4a5
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-05-11 14:11:02 +0700

    Get User by Id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { genSaltSync, hashSync } from 'bcryptjs';
@@ -29,8 +29,9 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) return 'Not found user !';
+    return this.userModel.findOne({ _id: id });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
