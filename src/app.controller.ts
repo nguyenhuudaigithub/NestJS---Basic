5f832e611176b292085e7bcb28bf9640d881d9eb
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-05-12 09:50:15 +0700

    JWT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,17 +2,19 @@ import { ConfigService } from '@nestjs/config';
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService,
   ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
-  handleLogin(@Request() req) {
-    return req.user;
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
