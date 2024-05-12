3a2dedd231176b727eed4c2c7a542c26d5cdcf77
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-05-12 09:51:31 +0700

    update jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secretOrPrivateKey: configService.get<string>('JWT_SECRET'),
+        privateKey: configService.get<string>('JWT_SECRET'),
         signOptions: {
           expiresIn: configService.get<string>('JWT_EXPIRE'),
         },
