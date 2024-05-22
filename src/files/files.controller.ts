3509f2c3e77efbc7c790e0615497f5721c40b090
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-05-22 15:04:50 +0700

    Upload File

---
+++ src/files/files.controller.ts
@@ -0,0 +1,68 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipeBuilder,
+  HttpStatus,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { CreateFileDto } from './dto/create-file.dto';
+import { UpdateFileDto } from './dto/update-file.dto';
+import { Public, ResponseMessage } from 'src/decorator/customize';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Public()
+  @Post('upload')
+  @ResponseMessage('Tải dữ liệu lên thành công !')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType:
+            /^(jpg|jpeg|png|image\/png|gif|txt|pdf|application\/pdf|doc|docx|text\/plain)$/i,
+        })
+        .addMaxSizeValidator({
+          maxSize: 1024 * 1024,
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return {
+      filename: file.filename,
+    };
+  }
+
+  @Get()
+  findAll() {
+    return this.filesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.filesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateFileDto: UpdateFileDto) {
+    return this.filesService.update(+id, updateFileDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.filesService.remove(+id);
+  }
+}
